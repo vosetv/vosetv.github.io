13a27443cc021017d4fd5412e06f34606f2d9bce
Simon Laroche <hi@simon.lc>
2016-08-13 19:22:45 -0400

    Okay figured out caching behavior is from nginx not express

--- app/server/index.js
+++ app/server/index.js
@@ -13,8 +13,7 @@ const port = process.env.PORT;
  */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.set('view cache', true);
-app.disable('view cache');
+app.set('view cache', true);
 
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy');
