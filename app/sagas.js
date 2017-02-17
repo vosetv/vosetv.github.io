39d02d040f7fe21f492a3e0331131f64f0e030da
Simon Laroche <hi@simon.lc>
2017-02-17 15:22:59 -0500

    Try to make change filter work as it should

--- app/sagas.js
+++ app/sagas.js
@@ -1,41 +1,54 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, select , takeLatest, takeEvery } from 'redux-saga/effects';
 import {
   REQUEST_VIDEOS,
   WATCH_VIDEO,
+  CHANGE_FILTER,
   receiveVideos,
 } from './actions';
 
 function* fetchVideos(action) {
-  const { subreddit, sort } = action.filter;
+  const {
+    subreddit = select(state => state.filter.subreddit),
+    sort = select(state => state.filter.sort),
+  } = action.filter;
   const response = yield call(fetch, `/api/videos/${subreddit}/${sort}`, { method: 'GET' });
   const videos = yield response.json();
   yield put(receiveVideos(videos));
 }
 
 function* watchVideo({ id }) {
-  const watchedVideos = []; // TODO Get watchedvideos
+  const watchedVideos = select(state => state.watchedVideos); // TODO Get watchedvideos
   if (watchedVideos.includes(id) === false) {
     watchedVideos.push(id);
   }
+
   // Make sure we don't go over our local storage limit
   if (watchedVideos.length > 50000) {
-    // TODO Splice array.
+    if (localStorage.getItem('watchedVideos') !== null) {
+      videos = JSON.parse(localStorage.getItem('watchedVideos'));
+    }
     watchedVideos.splice(0, 25000);
   }
+
   localStorage.setItem('watchedVideos', JSON.stringify(watchedVideos));
 }
 
-function* watchFetchVideos() {
+function* requestVideosWatcher() {
   yield takeLatest(REQUEST_VIDEOS, fetchVideos);
 }
 
-function* watchWatchVideo() {
-  yield takeLatest(WATCH_VIDEO, watchVideo);
+function* watchVideoWatcher() {
+  yield takeEvery(WATCH_VIDEO, watchVideo);
+}
+
+function* changeFilterWatcher() {
+  yield takeLatest(CHANGE_FILTER, fetchVideos);
 }
 
 export default function* rootSaga() {
   yield [
-    watchFetchVideos(),
-    watchWatchVideo(),
+    requestVideosWatcher(),
+    watchVideoWatcher(),
+    changeFilterWatcher(),
   ];
 }
