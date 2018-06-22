e83164e385e59360567f279887eaab8bada64ea0
Simon Laroche <simon.m.laroche@gmail.com>
2018-06-21 20:17:20 -0400

    Add filter bar

---
+++ src/scripts/components/filter.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Filter = () =>
+  <div className="filter">
+    <div className="sort__text">Filter</div>
+  </div>
+
+export default Filter;
