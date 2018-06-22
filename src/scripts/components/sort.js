dda45119e72deddd565046440efd0a1c737f6f90
Simon Laroche <hi@simon.lc>
2018-06-21 23:57:37 -0400

    Add unstated

--- src/scripts/components/sort.js
+++ src/scripts/components/sort.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Subscribe } from 'unstated';
 import objstr from 'obj-str';
+import State from './state';
 
 const Sort = () => (
-  <div className="sort">
-    <div className="sort__text">Sort</div>
-    <button className="button sort__button">videos</button>
-    <button className="button sort__button">Hot</button>
-  </div>
+  <Subscribe to={[State]}>
+    {({ state }) =>
+      <div className="sort">
+        <div className="sort__text">Sort</div>
+        <button className="button sort__button">{state.subreddit}</button>
+        <button className="button sort__button">{state.sort}</button>
+      </div>
+    }
+  </Subscribe>
 );
 
 export default Sort;
