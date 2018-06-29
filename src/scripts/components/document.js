19c6b7f0d9e3d3243151b512614bd0961d910f03
Simon Laroche <hi@simon.lc>
2018-06-29 14:20:54 -0400

    Vose.tv is cool

--- src/scripts/components/document.js
+++ src/scripts/components/document.js
@@ -25,69 +25,71 @@ const Document = () => (
                 <p><b>We couldn't find any videos for you...</b></p>
                 <p>Try a different sorting, or some of our favorite subreddits:</p>
                 <ul>
-                  <li><Link to="/r/artisanvideos">/r/artisanvideos</Link></li>
-                  <li><Link to="/r/gaming">/r/gaming</Link></li>
-                  <li><Link to="/r/movies">/r/movies</Link></li>
-                  <li><Link to="/r/shittyrobots">/r/shittyrobots</Link></li>
-                  <li><Link to="/r/Documentaries">/r/Documentaries</Link></li>
-                  <li><Link to="/r/WhyWereTheyFilming">/r/WhyWereTheyFilming</Link></li>
-                  <li><Link to="/r/Games">/r/Games</Link></li>
-                  <li><Link to="/r/space">/r/space</Link></li>
-                  <li><Link to="/r/Cyberpunk">/r/Cyberpunk</Link></li>
-                  <li><Link to="/r/youtubehaiku">/r/youtubehaiku</Link></li>
-                  <li><Link to="/r/MovieDetails">/r/MovieDetails</Link></li>
-                  <li><Link to="/r/DIY">/r/DIY</Link></li>
-                  <li><Link to="/r/Music">/r/Music</Link></li>
-                  <li><Link to="/r/Roadcam">/r/Roadcam</Link></li>
-                  <li><Link to="/r/holdmybeer">/r/holdmybeer</Link></li>
-                  <li><Link to="/r/holdmyredbull">/r/holdmyredbull</Link></li>
-                  <li><Link to="/r/holdmycosmo">/r/holdmycosmo</Link></li>
-                  <li><Link to="/r/holdmyfries">/r/holdmyfries</Link></li>
-                  <li><Link to="/r/whatcouldgowrong">/r/whatcouldgowrong</Link></li>
-                  <li><Link to="/r/LivestreamFail">/r/LivestreamFail</Link></li>
-                  <li><Link to="/r/hiphopheads">/r/hiphopheads</Link></li>
-                  <li><Link to="/r/ContagiousLaughter">/r/ContagiousLaughter</Link></li>
-                  <li><Link to="/r/PublicFreakout">/r/PublicFreakout</Link></li>
-                  <li><Link to="/r/cars">/r/cars</Link></li>
-                  <li><Link to="/r/DeepIntoYouTube">/r/DeepIntoYouTube</Link></li>
-                  <li><Link to="/r/CatastrophicFailure">/r/CatastrophicFailure</Link></li>
-                  <li><Link to="/r/PlayItAgainSam">/r/PlayItAgainSam</Link></li>
-                  <li><Link to="/r/AccidentalComedy">/r/AccidentalComedy</Link></li>
-                  <li><Link to="/r/amibeingdetained">/r/amibeingdetained</Link></li>
-                  <li><Link to="/r/ArtisanVideos">/r/ArtisanVideos</Link></li>
-                  <li><Link to="/r/AwfulCommercials">/r/AwfulCommercials</Link></li>
-                  <li><Link to="/r/BestOfLiveleak">/r/BestOfLiveleak</Link></li>
-                  <li><Link to="/r/bestofworldstar">/r/bestofworldstar</Link></li>
-                  <li><Link to="/r/cringe">/r/cringe</Link></li>
-                  <li><Link to="/r/CommercialCuts">/r/CommercialCuts</Link></li>
-                  <li><Link to="/r/cookingvideos">/r/cookingvideos</Link></li>
-                  <li><Link to="/r/curiousvideos">/r/curiousvideos</Link></li>
-                  <li><Link to="/r/educativevideos">/r/educativevideos</Link></li>
-                  <li><Link to="/r/FastWorkers">/r/FastWorkers</Link></li>
-                  <li><Link to="/r/fightporn">/r/fightporn</Link></li>
-                  <li><Link to="/r/FuckingWithNature">/r/FuckingWithNature</Link></li>
-                  <li><Link to="/r/fullmoviesonyoutube">/r/fullmoviesonyoutube</Link></li>
-                  <li><Link to="/r/happycrowds">/r/happycrowds</Link></li>
-                  <li><Link to="/r/idiotsfightingthings">/r/idiotsfightingthings</Link></li>
-                  <li><Link to="/r/IMGXXXX">/r/IMGXXXX</Link></li>
-                  <li><Link to="/r/lectures">/r/lectures</Link></li>
-                  <li><Link to="/r/mealtimevideos">/r/mealtimevideos</Link></li>
-                  <li><Link to="/r/motivationvideos">/r/motivationvideos</Link></li>
-                  <li><Link to="/r/ObscureMedia">/r/ObscureMedia</Link></li>
-                  <li><Link to="/r/playitagainsam">/r/playitagainsam</Link></li>
-                  <li><Link to="/r/Prematurecelebration">/r/Prematurecelebration</Link></li>
-                  <li><Link to="/r/streetfights">/r/streetfights</Link></li>
-                  <li><Link to="/r/sweetjustice">/r/sweetjustice</Link></li>
-                  <li><Link to="/r/TheWayWeWereOnVideo">/r/TheWayWeWereOnVideo</Link></li>
-                  <li><Link to="/r/trailers">/r/trailers</Link></li>
-                  <li><Link to="/r/UnexpectedThugLife">/r/UnexpectedThugLife</Link></li>
-                  <li><Link to="/r/video">/r/video</Link></li>
-                  <li><Link to="/r/videoporn">/r/videoporn</Link></li>
-                  <li><Link to="/r/vids">/r/vids</Link></li>
-                  <li><Link to="/r/vine">/r/vine</Link></li>
-                  <li><Link to="/r/vines">/r/vines</Link></li>
-                  <li><Link to="/r/virtualfreakout">/r/virtualfreakout</Link></li>
-                  <li><Link to="/r/woahtube">/r/woahtube</Link></li>
+                  {[
+                    '/r/artisanvideos',
+                    '/r/gaming',
+                    '/r/movies',
+                    '/r/shittyrobots',
+                    '/r/Documentaries',
+                    '/r/WhyWereTheyFilming',
+                    '/r/Games',
+                    '/r/space',
+                    '/r/Cyberpunk',
+                    '/r/youtubehaiku',
+                    '/r/MovieDetails',
+                    '/r/DIY',
+                    '/r/Music',
+                    '/r/Roadcam',
+                    '/r/holdmybeer',
+                    '/r/holdmyredbull',
+                    '/r/holdmycosmo',
+                    '/r/holdmyfries',
+                    '/r/whatcouldgowrong',
+                    '/r/LivestreamFail',
+                    '/r/hiphopheads',
+                    '/r/ContagiousLaughter',
+                    '/r/PublicFreakout',
+                    '/r/cars',
+                    '/r/DeepIntoYouTube',
+                    '/r/CatastrophicFailure',
+                    '/r/PlayItAgainSam',
+                    '/r/AccidentalComedy',
+                    '/r/amibeingdetained',
+                    '/r/ArtisanVideos',
+                    '/r/AwfulCommercials',
+                    '/r/BestOfLiveleak',
+                    '/r/bestofworldstar',
+                    '/r/cringe',
+                    '/r/CommercialCuts',
+                    '/r/cookingvideos',
+                    '/r/curiousvideos',
+                    '/r/educativevideos',
+                    '/r/FastWorkers',
+                    '/r/fightporn',
+                    '/r/FuckingWithNature',
+                    '/r/fullmoviesonyoutube',
+                    '/r/happycrowds',
+                    '/r/idiotsfightingthings',
+                    '/r/IMGXXXX',
+                    '/r/lectures',
+                    '/r/mealtimevideos',
+                    '/r/motivationvideos',
+                    '/r/ObscureMedia',
+                    '/r/playitagainsam',
+                    '/r/Prematurecelebration',
+                    '/r/streetfights',
+                    '/r/sweetjustice',
+                    '/r/TheWayWeWereOnVideo',
+                    '/r/trailers',
+                    '/r/UnexpectedThugLife',
+                    '/r/video',
+                    '/r/videoporn',
+                    '/r/vids',
+                    '/r/vine',
+                    '/r/vines',
+                    '/r/virtualfreakout',
+                    '/r/woahtube',
+                  ].map(link => <li><Link to={link}>{link}</Link></li>)}
                 </ul>
               </div>
             </div>
