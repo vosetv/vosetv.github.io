163bd1680cdc1d671b37b24c50355d8d1c6b9032
Simon Laroche <hi@simon.lc>
2018-06-28 22:07:36 -0400

    Updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
-import Loadable from 'react-loadable';
 import Document from './components/document';
 
-window.main = () => {
-  Loadable.preloadReady().then(() => {
-    const renderMethod = !!module.hot ? render : hydrate;
-    renderMethod(<Document />, document.getElementById('root'));
-  });
-};
+const renderMethod = !!module.hot ? render : hydrate;
+renderMethod(<Document />, document.getElementById('root'));
