4e5da1d78989a8bd22e3c23f38610e144bd23060
Simon Laroche <hi@simon.lc>
2019-03-31 20:48:35 -0400

    Update deps and fix hooks

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,38 +1,28 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import Observer from 'react-intersection-observer';
 import PropTypes from 'prop-types';
 
-// Image name conflicts with browser Image constructor
-export default class ImageComponent extends PureComponent {
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string,
-  };
-  state = {
-    loaded: false,
-  };
-  loadImage = inView => {
-    if (!inView || this.state.loaded) return;
-    // console.log('load image');
+export default function ImageComponent({ src, ...props }) {
+  const [loaded, setLoaded] = useState(false);
+  function loadImage(inView) {
+    if (!inView || loaded) return;
     // TODO Handle 404s
     const image = new Image();
-    image.onload = () => this.setState({ loaded: true });
-    image.src = this.props.src;
-  };
-  render() {
-    return (
-      <Observer onChange={this.loadImage} rootMargin="0px 0px 500px">
-        {({ ref }) =>
-          this.state.loaded ? (
-            <img ref={ref} {...this.props} />
-          ) : (
-            <div
-              ref={ref}
-              className="video-item__thumb video-item__thumb--preview"
-            />
-          )
-        }
-      </Observer>
-    );
+    image.onload = () => setLoaded(true);
+    image.src = src;
   }
+  return (
+    <Observer onChange={loadImage} rootMargin="0px 0px 500px">
+      {({ ref }) =>
+        loaded ? (
+          <img ref={ref} src={src} {...props} />
+        ) : (
+          <div
+            ref={ref}
+            className="video-item__thumb video-item__thumb--preview"
+          />
+        )
+      }
+    </Observer>
+  );
 }
--- src/components/video-item/index.js
+++ src/components/video-item/index.js
@@ -1,47 +1,31 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import objstr from 'obj-str';
 import Image from '../image';
 import './styles.css';
 
-export default class VideoItem extends PureComponent {
-  ref = React.createRef();
-
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.isSelected &&
-      prevProps.isSelected !== this.props.isSelected
-    ) {
-      this.scrollIntoView();
-    }
-  }
-
-  scrollIntoView() {
-    this.ref.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'nearest',
-    });
-  }
-
-  render() {
-    const { id, title, isSelected, isWatched, onClick } = this.props;
-    const classes = objstr({
-      'video-item': true,
-      'video-item--selected': isSelected,
-      'video-item--watched': isWatched,
-    });
-    return (
-      <li ref={this.ref} className={classes} onClick={onClick}>
-        <Image
-          className="video-item__thumb"
-          src={`https://i.ytimg.com/vi/${id}/default.jpg`}
-          alt=""
-        />
-        <div className="video-item__title">{title}</div>
-      </li>
-    );
-  }
+export default function VideoItem({
+  id,
+  title,
+  isSelected,
+  isWatched,
+  onClick,
+}) {
+  const classes = objstr({
+    'video-item': true,
+    'video-item--selected': isSelected,
+    'video-item--watched': isWatched,
+  });
+  return (
+    <li className={classes} onClick={onClick}>
+      <Image
+        className="video-item__thumb"
+        src={`https://i.ytimg.com/vi/${id}/default.jpg`}
+        alt=""
+      />
+      <div className="video-item__title">{title}</div>
+    </li>
+  );
 }
 
 VideoItem.propTypes = {
