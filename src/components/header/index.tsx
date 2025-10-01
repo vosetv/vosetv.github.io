160b6a107209ac2251e570cbe4ee9ca30b61dd23
Simon Laroche <hi@simon.lc>
2025-10-01 19:03:25 -0400

    Fix case sensitive issue

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../logo';
-import Link from '../Link';
+import Link from '../link';
 import styles from './styles.module.css';
 
 export default function Header({ children }) {
